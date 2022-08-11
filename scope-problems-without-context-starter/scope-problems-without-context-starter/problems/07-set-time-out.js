4a2a1cc128ef7fc00f47f366508d1029105ba986
nick <nickadaniya@gmail.com>
2022-08-11 12:37:24 -1000

    problems 7-10

--- scope-problems-without-context-starter/scope-problems-without-context-starter/problems/07-set-time-out.js
+++ scope-problems-without-context-starter/scope-problems-without-context-starter/problems/07-set-time-out.js
@@ -2,25 +2,23 @@
 Write a function named `funcTimer(time, func)` that will allow you to hand
 in a function and a number representing milliseconds. The `funcTimer` should use
 the global.setTimeout function to invoke the passed in function in `time` amount
-of seconds. 
+of seconds.
 
 There are no specs for this problem - try it in the console yourself to
 test your answer!
 
 Examples:
 
-function partyFunc () {
-  console.log("Party time!")
-}
-
-funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 
 ***********************************************************************/
 
 const funcTimer = (time, func) => {
-  setTimeout()
-  
+  setTimeout(func, time)
+}
+
+function partyFunc () {
+  console.log("Party time!")
 }
 
 function partyFunc () {
@@ -36,5 +34,3 @@ try {
   // catch the ref err
   module.exports = null;
 }
-
-
--- scope-problems-without-context-starter/scope-problems-without-context-starter/problems/09-interrupter.js
+++ scope-problems-without-context-starter/scope-problems-without-context-starter/problems/09-interrupter.js
@@ -4,20 +4,25 @@ accept a word and return a function. When the function returned by interrupter
 is invoked with a string the string will be returned with "interruptions".
 
 Look below to see how this function is invoked:
-let rudePerson = interrupter("what"); // => returns a function
-console.log(rudePerson("how are you")); // prints "how what are what you"
-console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
-let rudePerson2 = interrupter("yo"); // => returns a function
-console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
+Invoking the interrupter function again:
 
 
 ***********************************************************************/
 
-// your code here!
+const interrupter = (interruptingWord) => {
+  return function person(word) {
+    let words = word.split(' ')
+    return words.join(' ' + interruptingWord + ' ')
+  }
+}
 
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
