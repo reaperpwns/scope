0382511a7916b875f42820dd27298a959e7dd5f3
Mike Miller <millermichaelg1@gmail.com>
2022-08-11 16:15:00 -0500

    WE REALLY REALLY DID IT FAM

--- scope-problems-without-context-starter/scope-problems-without-context-starter/problems/06-closure-sandwiches.js
+++ scope-problems-without-context-starter/scope-problems-without-context-starter/problems/06-closure-sandwiches.js
@@ -16,8 +16,22 @@ let sandwich2 = sandwichMaker(); // => returns a function
 sandwich2("pb") // => "One sandwich with tomato and pb"
 
 ***********************************************************************/
+const sandwichMaker = () => {
+  let sandwich = `One sandwich with tomato`
+  function ingredients(ingredient) {
+    return sandwich += ' and ' + ingredient
+  }
+  return ingredients
+}
 
-// your code here!
+let sandwich = sandwichMaker(); // => returns a function
+console.log(sandwich("spinach")) // => "One sandwich with tomato and spinach"
+console.log(sandwich("jelly")) // => "One sandwich with tomato and spinach and jelly"
+console.log(sandwich("bread")) // => "One sandwich with tomato and spinach and jelly and bread"
+
+// Another Sandwich:
+let sandwich2 = sandwichMaker(); // => returns a function
+console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
